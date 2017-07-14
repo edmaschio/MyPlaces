a1e3d13ffa9befc231c54e99a209de9a9ce0752b
Ederson Tadeu Maschio <edmaschio@gmail.com>
2017-07-13 23:22:45 -0300

    Change config page

---
+++ src/config/environment.dev-template.ts
@@ -0,0 +1,3 @@
+var config = {
+    API_KEY: 'AIzaSyBM826GrUEqdwe1iDsYqtT5aUQfebwh9eM'
+}
---
+++ src/config/environment.prod.ts
@@ -0,0 +1,5 @@
+export const ENV = {  
+  PRODUCTION : false,
+  API_URL    : 'prod.local',
+  GMAPS_API_KEY: 'AIzaSyBM826GrUEqdwe1iDsYqtT5aUQfebwh9eM'
+};
