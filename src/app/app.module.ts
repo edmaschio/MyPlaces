1ad0eb31c6fee40aba02fa5e0b4d99243a230fc1
Ederson Tadeu Maschio <edmaschio@gmail.com>
2017-07-19 23:38:25 -0300

    Remove data from storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AddPlacePage } from "../pages/add-place/add-place";
 import { PlacePage } from "../pages/place/place";
 import { SetLocationPage } from "../pages/set-location/set-location";
 import { PlacesService } from "../services/places";
+import { Storage } from "@ionic/storage";
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { PlacesService } from "../services/places";
     Geolocation,
     Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    PlacesService
+    PlacesService, 
+    Storage
   ]
 })
 export class AppModule {}
--- src/services/places.ts
+++ src/services/places.ts
@@ -2,13 +2,14 @@ import { Place } from "../models/place";
 import { Location } from "../models/location";
 
 import { Storage } from "@ionic/storage";
+import { File } from "@ionic-native/file";
 import { Injectable } from "@angular/core";
 
 @Injectable()
 export class PlacesService {
     private places: Place[] = [];
 
-    constructor(private storage: Storage) { }
+    constructor(private storage: Storage, private file: File) { }
 
     addPlace(title: string, descriprion: string, location: Location, imageUrl: string) {
         const place = new Place(title, descriprion, location, imageUrl);
@@ -38,6 +39,32 @@ export class PlacesService {
     }
 
     deletePlace(index: number) {
+        const place = this.places[index];
         this.places.splice(index, 1);
+        this.storage.set('places', this.places)
+            .then(
+                () => {
+                    this.removeFile(place);
+                }
+            )
+            .catch(
+                err => console.log(err)
+            )
+    }
+
+    private removeFile(place: Place) {
+        const currentName = place.imageUrl.replace(/^.*[\\\/]/, '');
+        this.file.removeFile(this.file.dataDirectory, currentName)
+            .then(
+                () => console.log('Arquivo removido')
+            )
+            .catch(
+                () => {
+                    console.log('Erro ao remover o arquivo');
+                    this.addPlace(place.title, place.description, place.location, place.imageUrl);
+                }
+            )
+
+            ;
     }
 }
