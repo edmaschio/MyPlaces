f0f9dd36392adc78c610b500dff05c6b5841f0fd
Ederson Tadeu Maschio <edmaschio@gmail.com>
2017-07-19 23:29:55 -0300

    Added save file on storage and in service

--- src/services/places.ts
+++ src/services/places.ts
@@ -1,16 +1,40 @@
 import { Place } from "../models/place";
 import { Location } from "../models/location";
 
+import { Storage } from "@ionic/storage";
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class PlacesService {
     private places: Place[] = [];
+
+    constructor(private storage: Storage) { }
 
     addPlace(title: string, descriprion: string, location: Location, imageUrl: string) {
         const place = new Place(title, descriprion, location, imageUrl);
         this.places.push(place);
+        this.storage.set('places', this.places)
+            .then()
+            .catch(
+            err => {
+                this.places.splice(this.places.indexOf(place), 1);
+            }
+            )
     }
 
     loadPlaces() {
         return this.places.slice();
+    }
+
+    fetchPlaces() {
+        this.storage.get('places')
+            .then(
+            (places: Place[]) => {
+                this.places = places != null ? places : [];
+            })
+            .catch( 
+            err => console.log(err)
+            );
     }
 
     deletePlace(index: number) {
